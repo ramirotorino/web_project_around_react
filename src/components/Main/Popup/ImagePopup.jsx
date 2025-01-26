d52d76482d1bc76dec02190aa2e315a385dfe787
ramirotorino <ramirotorino@hotmail.com>
2025-01-26 11:43:02 +1000

    correccion de boton de imagen

--- src/components/Main/Popup/ImagePopup.jsx
+++ src/components/Main/Popup/ImagePopup.jsx
@@ -1,36 +1,24 @@
-// Debugged ImagePopup.jsx
 import React from "react";
 import "@/blocks/popup.css";
 import closeIcon from "../../../images/CloseIcon.svg";
 
 export default function ImagePopup({ card, isOpen, onClose }) {
   return (
-    <div className={`popup ${isOpen ? "popup__opened" : ""}`}>
+    <div className={`popup image-popup ${isOpen ? "popup__opened" : ""}`}>
       <div className="popup__content popup__content_content_image">
-        <button
-          style={{
-            position: "relative",
-            top: "-8px",
-            right: "-535px",
-            background: "transparent",
-            border: "none",
-            fontSize: "20px",
-            cursor: "pointer",
-          }}
-          className="popup__close-button"
-          type="button"
-          onClick={() => {
-            onClose();
-          }}
-          aria-label="Close image popup"
-        >
-          <img src={closeIcon} alt="Close popup" />
-        </button>
         {card ? (
-          <>
+          <div className="popup__image-container">
+            <button
+              className="popup__close-button image-popup__close-button"
+              type="button"
+              onClick={onClose}
+              aria-label="Close image popup"
+            >
+              <img src={closeIcon} alt="Close popup" />
+            </button>
             <img className="popup__image" src={card.link} alt={card.name} />
             <p className="popup__caption">{card.name}</p>
-          </>
+          </div>
         ) : (
           <p>No se seleccionó ninguna tarjeta</p>
         )}
